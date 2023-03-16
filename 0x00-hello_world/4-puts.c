c5625268c12a088a022023ea39af88253d4a3e64
Michael Achumba <michaelachumba@gmail.com>
2023-03-16 06:20:07 -0700

    Script to print a string using puts followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char stng[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(stng);
+
+	return (0);
+}
