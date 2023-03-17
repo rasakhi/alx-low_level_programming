3089ad89c35b1ad8da78f5323d71617d9227f4a1
Michael Achumba <michaelachumba@gmail.com>
2023-03-17 16:47:12 -0700

    C program that prints all singlr digits in base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char dgt_10 = '0';
+
+	while (dgt_10 <= '9')
+	{
+		putchar(dgt_10);
+		dgt_10++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
