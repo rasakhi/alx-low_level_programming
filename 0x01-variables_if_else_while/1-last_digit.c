f937267cf73edc944eded5c5099e9484ff6482d3
Michael Achumba <michaelachumba@gmail.com>
2023-03-17 15:43:16 -0700

    C program to assign random number to n and print the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	i = n % 10;
+
+	if (i > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	} else if (i == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	} else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	}
+
+	return (0);
+}
