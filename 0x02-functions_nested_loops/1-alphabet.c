15921ae8c0a03b5bac3951de633ad8285dee498f
Michael Achumba <michaelachumba@gmail.com>
2023-03-21 09:12:07 -0700

    c function to print all lowercase alphabets - take 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - c function that prints the alphabet, in lowercase
+ * print_alphabet -  c function that prints the alphabet, in lowercase
  *
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -16,6 +15,4 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
-
-	return;
 }
