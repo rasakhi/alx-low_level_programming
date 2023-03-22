b3766da0d58b67ae68b34649cbeb1971ad92ce8f
Michael Achumba <michaelachumba@gmail.com>
2023-03-22 02:49:05 -0700

    function to print last digit of a number - take 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,9 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		last_digit = last_digit * 1;
+		last_digit = last_digit * -1;
 	}
 
-	_putchar(last_digit + '0');
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
