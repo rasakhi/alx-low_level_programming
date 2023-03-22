8959bee2cff5df9273a1213e1a56c300beebf91c
Michael Achumba <michaelachumba@gmail.com>
2023-03-22 03:32:40 -0700

    function that prints every minute of the day of Jack Bauer - take 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,18 +9,21 @@ void jack_bauer(void)
 
 	for (a = '0' ; a < '3' ; a++)
 	{
-	for (b = '0' ; b < '4' ; b++)
+	for (b = '0' ; b < '9' ; b++)
 	{
 	for (c = '0' ; c < '6' ; c++)
 	{
 	for (d = '0' ; d <= '9' ; d++)
 	{
-		_putchar(a);
-		_putchar(b);
-		_putchar(':');
-		_putchar(c);
-		_putchar(d);
-		_putchar('\n');
+		if (a < '3' && b < '4')
+		{
+			_putchar(a);
+			_putchar(b);
+			_putchar(':');
+			_putchar(c);
+			_putchar(d);
+			_putchar('\n');
+		}
 	}
 	}
 	}
