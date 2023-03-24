a5a454453b339d009d2e5175d5c8665761c164f0
Michael Achumba <michaelachumba@gmail.com>
2023-03-23 18:54:10 -0700

    c program that prints numbers 1 to 100 but multiples of 3 print Fizz, 5 Buzz and 15 FizzBuzz - take 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,24 @@ int main(void)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-
+		if (i != 100)
+		{
+			printf(" ");
+		}
 		i++;
 	}
 
