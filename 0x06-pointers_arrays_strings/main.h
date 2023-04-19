c4a247eb37c75de25e5ba48981628037b8e8e22d
Michael Achumba <michaelachumba@gmail.com>
2023-04-19 02:22:11 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,15 +5,15 @@
  */
 int _putchar(char c);
 /**
- * *_strcat - concatenates two strings
+ * _strcat - concatenates two strings
  */
 char *_strcat(char *dest, char *src);
 /**
- * *_strncat - concatenates two strings
+ * _strncat - concatenates two strings
  */
 char *_strncat(char *dest, char *src, int n);
 /**
- * *_strncpy - copies a string
+ * _strncpy - copies a string
  */
 char *_strncpy(char *dest, char *src, int n);
 /**
@@ -25,12 +25,12 @@ int _strcmp(char *s1, char *s2);
  */
 void reverse_array(int *a, int n);
 /**
- * *string_toupper - changes all lowercase letters
+ * string_toupper - changes all lowercase letters
  * of a string to uppercase
  */
 char *string_toupper(char *);
 /**
- * *cap_string - capitalizes all words of a string
+ * cap_string - capitalizes all words of a string
  */
 char *cap_string(char *);
 /**
