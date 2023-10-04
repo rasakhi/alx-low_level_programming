73ccaa8774989c0efc622fda18f577c597c5cf6b
Michael Achumba <michaelachumba@gmail.com>
2023-10-04 12:00:32 -0700

    Program to allocate memory to a grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates memory to 2 dimensional array
+ * @width: width of array
+ * @height: height of array
+ * Return: pointer to grid or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, w, h;
+	int **a;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	for (h = 0; h < height; h++)
+	{
+		a[h] = malloc(sizeof(int) * width);
+		if (a[h] == NULL)
+		{
+			for (w = h; w >= 0; w--)
+				free(a[w]);
+			free(a);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			a[i][j] = 0;
+	}
+	return (a);
+}
