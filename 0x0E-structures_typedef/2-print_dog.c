bd0e5c6a6c2498d6609e691a4a083b153aae99a6
Michael Achumba <michaelachumba@gmail.com>
2023-10-11 15:38:19 -0700

    Program that prints a struct dog - Final take

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (!(d->age))
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else
