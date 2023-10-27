eed19ca83bb9fe9949df10790dfcc3565e86b3a6
Michael Achumba <michaelachumba@gmail.com>
2023-10-27 03:49:43 -0700

    Fix betty errors in code

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -10,11 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int integer;
 	int length, binary;
-	
+
 	if (!b)
 		return (0);
 	integer = 0;
-	for (length = 0; b[length] != '\0'; length++);
+	for (length = 0; b[length] != '\0'; length++)
+		;
 	for (length--, binary = 1; length >= 0; length--, binary *= 2)
 	{
 		if (b[length] != '0' && b[length] != '1')
